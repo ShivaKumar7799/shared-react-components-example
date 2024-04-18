6a90c8b2ec66a5befe50ffc3d4a02c6ac7c692be
Shiva <shiva.bammidi@neosoftmail.com>
2024-04-18 11:41:41 +0530

    heading component updated

--- src/TodaysDate.js
+++ src/TodaysDate.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const TodaysDate = () => <div>{`Today's date : ${Date()}`}</div>;
+const TodaysDate = () => <div>{`Today's date is : ${Date()}`}</div>;
 
 export default TodaysDate;
